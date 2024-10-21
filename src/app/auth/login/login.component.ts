c9c90f22a6b7f689f6577784bc8ee9898a4ce685
Ivan <ivan.davila.2003@gmail.com>
2024-10-21 13:27:40 -0600

    styled login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ MatInputModule, MatButtonModule, MatCardModule ],
+  imports: [ MatInputModule, MatButtonModule, MatCardModule, MatIconModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
