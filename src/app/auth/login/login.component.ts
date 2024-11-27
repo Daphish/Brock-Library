bacc2a9fc4a5d5a5a1467c05481f4ad994768721
Daphish <ivan.davila.2003@gmail.com>
2024-11-26 20:30:42 -0600

    Login page created

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RippleModule } from 'primeng/ripple';
+import { PrimeNGConfig } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ ],
+  imports: [ ButtonModule, RippleModule, CheckboxModule, InputTextModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private primengConfig: PrimeNGConfig) { } 
+  
+    ngOnInit() { 
+        this.primengConfig.ripple = true; 
+    } 
 }
