e82f1b33415e33fc215f588606cb496acb2954c0
Daphish <ivan.davila.2003@gmail.com>
2024-12-01 01:58:43 -0600

    Segundo avance

--- src/app/books/inicio/inicio.component.ts
+++ src/app/books/inicio/inicio.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { PrimeNGConfig } from 'primeng/api';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [ ],
+  imports: [ CardModule, ButtonModule, RippleModule, NgFor ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  constructor(private primengConfig: PrimeNGConfig) { } 
+  
+    ngOnInit() { 
+        this.primengConfig.ripple = true; 
+    }
+  cards=Array(15);
 }
--- src/app/books/main-layout/main-layout.component.ts
+++ src/app/books/main-layout/main-layout.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, ToolbarModule, AvatarModule, RouterLink],
+  imports: [RouterOutlet, ToolbarModule, AvatarModule, RouterLink, IconFieldModule, InputIconModule, InputTextModule, FormsModule, DropdownModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent {
 
+  cities: City[] | undefined;
+
+  selectedCity: City | undefined;
+
+  ngOnInit() {
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' }
+      ];
+  }
 }
